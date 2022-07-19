1d203bba449e4e6748d843b16e478beccf68d042
CAMILA ROMERO <camilaar@byui.edu>
2022-07-19 13:50:17 -0400

    final

--- SleepOutside/src/js/cart.js
+++
@@ -1,7 +0,0 @@
-import { loadHeaderFooter } from "./utils.js";
-import CartList from "./cartList.js";
-
-loadHeaderFooter();
-
-const cart = new CartList("so-cart", document.querySelector(".product-list"));
-cart.init();
